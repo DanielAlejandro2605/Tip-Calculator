65c6a1a5bd57f70ae8f08f08517de48e058e76bc
Daniel Nieto <dani2600nieto@gmail.com>
2021-09-29 22:26:39 -0500

    Algorithm ready

--- tip-calculator-app-main/script/script.js
+++ tip-calculator-app-main/script/script.js
@@ -1,158 +1,93 @@
-// Getting the values for perform the operation
+let document_innerHTML = document.body.innerHTML;
 
-// See if there's a button selected:
-let aux;
-let button_selected;
-let percentage_options = document.getElementById('percentage_options');
-// Select a button 
-percentage_options.onclick = function(event){
-    let button = event.target.closest('button');
-    if(!button) return;
-    if(!percentage_options.contains(button)) return;
-    addClassSelected(button);
+function resetAutomatic(){
+    document.body.innerHTML = document_innerHTML;    
 }
-// Add class to the button
-function addClassSelected(button){
-    if(button_selected){
-        button_selected.classList.remove('selected');
-    }
-    button_selected = button;
-    button_selected.classList.add('selected');
-    aux = button_selected.innerHTML;
-}
-
-// Create the event for lauch the calculation    
-let action_button = document.getElementById("action_button");
-
-action_button.onclick = function(){
-    // Reset action
-    if(action_button.innerHTML == "Reset"){
-        resetValues();
-        // Changing the HTML of the button action
-        action_button.innerHTML = "Enter";
-    } else if(action_button.innerHTML == "Enter"){
-        tipCalculator();
-        // Changing the HTML of the button action
-        action_button.innerHTML = "Reset";
-    }
-};
-
-// Function to reset the values
-function resetValues(){
-    document.getElementById("bill_total").value = "";
-    document.getElementById("number_of_people").value = "";
-    document.getElementById('custom_percentage').value = "";
-    button_selected.classList.remove('selected');
-    let div_tip_amount = document.getElementById('tip_amount');
-    let div_total_per_person = document.getElementById('total_person');
-    div_tip_amount.innerHTML = "$0.00";
-    div_total_per_person.innerHTML = "$0.00";
-
-    document.querySelectorAll('.message').forEach(e => e.remove());
-}
-// Function to perform the operation
-function tipCalculator(){
-    let arrayData = gettingValuesReady();
-    let bill_total = arrayData[0];
-    let number_of_people = arrayData[1];
-    let percentage = arrayData[2];
-    if(bill_total != undefined && number_of_people != undefined && percentage != undefined){
-         // Compute tip amount
-        let tip_amount = (bill_total * percentage) / 100 * 2 * 0.1;
-        let div_tip_amount = document.getElementById('tip_amount');
-        // Compute the total per person
-        let total_per_person = (bill_total / number_of_people + tip_amount).toFixed(2);
-        let div_total_per_person = document.getElementById('total_person');
-
-        // Putting the data
-        div_tip_amount.innerHTML = myRound(tip_amount, 2);
-        div_total_per_person.innerHTML = total_per_person;
-    } else{
-        alert('Ya veremos que hacer');
-    }
-   
-}
-
-// Getting the values ready for operation
-function gettingValuesReady(){
-    let bill_total = document.getElementById("bill_total"); 
-    let number_of_people = document.getElementById("number_of_people");
+function verifyValues(){
+    let array = getValues();
+    let bill_total = array[0];
+    let percentage = array[1];
+    let number_of_people = array[2];
     
-    bill_total = verifyBillTotalField(bill_total);
-    number_of_people = verifyNumberOfPeopleField(number_of_people);
-    let percentage = verifyPercentageField();
-
-    return [bill_total, number_of_people, percentage];
-}
-// Function to verify the value of bill total field
-
-function verifyBillTotalField(bill_total){
-    // Verify if there is not value.
-    
-    if(bill_total.value == ""){
-        createMessage(bill_total, "Missing bill total value");
-        return;
-    } // Verify if there's a correct value.
-    else if(Object.is(NaN, Number(bill_total.value))){
-        createMessage(bill_total, "Bill total must be a number");
-        return;
-    }
-
-    return Number(bill_total.value);
-}
-
-// Function to verify the value of number of people field
-function verifyNumberOfPeopleField(number_of_people){
-    // Verify if there is not value.
-    if(number_of_people.value == ""){
-        createMessage(number_of_people, "Missing the number of people value")
-        return;
-    }
-    // Verify if there's a correct value.
-    else if(Object.is(NaN, Number(number_of_people.value))){
-        createMessage(number_of_people, "Number of people must be a number");
-        return;
-    }
-    return Number(number_of_people.value);
-}
-
-function verifyPercentageField(){
-    let percentage;
-    let custome_percentage = document.getElementById('custom_percentage');
-    if(custome_percentage.value == "" && aux != undefined){
-        percentage = aux.match(/\d+/g);
-    } else if(custome_percentage.value != "" && aux == undefined){
-        percentage = custome_percentage.value.match(/\d+/g);
-    } else if(custome_percentage.value != "" && aux != undefined){
-        createMessage(custome_percentage, "Select one button or custom percentage, not both");
-        return;
+    if(verifyBillTotalField(bill_total, percentage, number_of_people)){
+        bill_total = Number(bill_total);
+        percentage = Number(percentage);
+        number_of_people = Number(number_of_people);
+        tipCalculator(bill_total, percentage, number_of_people);
     } else{
-        createMessage(custome_percentage, "Missing percentage of tip");
-        return;
+        setTimeout(() => {
+            resetAutomatic();
+        }, 5000);
+    }
+}
+function putValues(tip_amount, total_per_person){
+    let div_tip_amount = document.getElementById('tip_amount_container');
+    let div_total_per_person = document.getElementById('total_per_person_container');
+    div_tip_amount.innerHTML = "$" + myRound(tip_amount, 2);
+    div_total_per_person.innerHTML = "$" + total_per_person;
+}
+function tipCalculator(bill_total, percentage, number_of_people){
+    let tip_amount = ((bill_total * percentage) / 100 * 2 * 0.1);
+    let total_per_person = (bill_total / number_of_people + tip_amount).toFixed(2);
+    putValues(tip_amount, total_per_person);
+}
+function verifyBillTotalField(bill_total, percentage, number_of_people){
+    let container_bill_total = document.getElementById('bill_total_container');
+    let container_percentage = document.getElementById('percentage_container');
+    let container_people = document.getElementById('people_container');
+    let flag = 0;
+    // Verify if there are correct values
+    if(Object.is(NaN, Number(bill_total))){
+        createMessage(container_bill_total, "It must be a number(non zero)");
+        flag = 1;
+    }
+    if(Object.is(NaN, Number(percentage))){
+        createMessage(container_percentage, "It must be a number(non zero)");
+        flag = 1;
+    }
+    if(Object.is(NaN, Number(number_of_people))){
+        createMessage(container_people, "It must be a number(non zero)");
+        flag = 1;
     }
 
-    return Number(percentage);
+    if(flag == 1){
+        return false;
+    }
+    return true;
+
 }
-// Function to create message alerts
+
+function getValues(){
+    let percentage_value;
+    let custom_value = document.getElementById('custom').value;
+    if(custom_value != ""){
+        percentage_value = custom_value;
+    } else{
+        percentage_value = getPercentageValue();
+    }
+    let bill_total_value = document.getElementById('fname').value;
+    let number_of_people_value = document.getElementById('email').value;
+    return [bill_total_value, percentage_value, number_of_people_value];   
+}
+
+function getPercentageValue(){
+    let percentage_aux;
+    let button_selected_value = document.querySelector("input[type='radio'][name='buttonGroup']:checked").value;
+    if(button_selected_value != undefined){
+        percentage_aux = button_selected_value;
+    }
+    return percentage_aux;
+}
 function createMessage(elem, html) {
-    // create message element
-    let message = document.createElement('div');
-    // alert(elem.parentNode);
-    // elem.parentNode.style.backgroundColor = 'red';
-    // better to use a css class for the style here
-    message.style.cssText = "position:fixed; color: red";
-  
-    // assign coordinates, don't forget "px"!
-    let coords = elem.getBoundingClientRect();
-  
-    message.style.left = coords.left + 40 + "px";
-    //message.style.top = coords.bottom + "px";
-    message.classList.add('message');
-    message.innerHTML = html;
-    elem.parentNode.append(message);
+    let paragraph = elem.firstElementChild;
+    let original_html = paragraph.innerHTML;
+    let span = `<span>${html}</span>`;
+    paragraph.innerHTML = paragraph.innerHTML + span; 
+    setTimeout(() => {
+        paragraph.innerHTML = original_html;
+    }, 4000);
 }   
-  
-// Function to round the values
+
 function myRound(num, dec) {
     let exp = Math.pow(10, dec || 2); // 2 decimales por defecto
     return parseInt(num * exp, 10) / exp;
